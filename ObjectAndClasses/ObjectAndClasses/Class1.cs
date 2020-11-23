c1646d6b0995a5576be02f759743b27d394eee27
khubaib-khalid-techverx <74600712+khubaib-khalid-techverx@users.noreply.github.com>
2020-11-23 13:51:35 +0500

    Add files via upload

---
+++ ObjectAndClasses/ObjectAndClasses/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectAndClasses
+{
+	public class Employee
+	{
+		private int id;
+		private string name;
+		private int age;
+		public Employee(int id, string name, int age)
+		{
+			this.id = id;
+			this.name = name;
+			this.age = age;
+
+		}
+		public void show()
+		{
+			Console.WriteLine(id + " " + name + " " + age);
+		}
+
+	}
+}
---
+++ ObjectAndClasses/ObjectAndClasses/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ObjectAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee(1,"Khubaib",20);
+
+            e.show();
+         
+        }
+    }
+}
